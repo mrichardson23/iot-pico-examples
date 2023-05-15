e9ccb9964ae7919478a442dfc54c624bc53ea684
Matt Richardson <mrichardson23@gmail.com>
2023-05-15 23:58:43 +0300

    Add WiFi connection code

---
+++ wificonnect.py
@@ -0,0 +1,22 @@
+import network
+import socket
+from time import sleep
+import machine
+
+ssid = 'Gearbox Members'
+password = 'password here'
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    print(wlan.ifconfig())
+
+try:
+    connect()
+except KeyboardInterrupt:
+    machine.reset()
